1ac68922d5f9c3c8d4517e711ca7124ced931af8
guz86 <guzmo6@rambler.ru>
2021-07-02 04:51:34 +0300

    example

---
+++ inkrementDecrement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inkrementDecrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            // постфиксная форма инкремента и декремента
+            
+            Console.WriteLine("a++ "+ a++); 
+            Console.WriteLine("a-- " + a--);
+
+            // префиксная форма инкремента и декремента
+            Console.WriteLine("++a "+ ++a);
+            Console.WriteLine("--a " + --a);
+
+        }
+    }
+}
