1b894922638b4293f76798ff4167c5229e6c7721
guz86 <guzmo6@rambler.ru>
2021-07-15 17:26:08 +0300

    ifElse

---
+++ ifElse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ifElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isInfected;
+            Random rand = new Random();
+            
+            // isInfected = rand.Next(0, 2) == 0;
+
+            if (rand.Next(0, 2) == 0)
+            {
+                Console.WriteLine("Вы подхватили инфекцию!");
+            }
+            else
+            {
+                Console.WriteLine("Вы здоровы!");
+            }
+        }
+    }
+}
