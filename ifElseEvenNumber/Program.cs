80a7e4096ec19d5b0068125c4bb4187f5fa5128f
guz86 <guzmo6@rambler.ru>
2021-07-15 17:34:21 +0300

    ifElseEvenNumber

---
+++ ifElseEvenNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ifElseEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int enterNumber;
+            Console.Write("Введите число: ");
+            enterNumber = Convert.ToInt32( Console.ReadLine());
+            if (enterNumber % 2 == 0 )
+            {
+                Console.WriteLine("Это четное число!");
+            }
+            else
+            {
+                Console.WriteLine("Это нечетное число!");
+            }
+        }
+    }
+}
