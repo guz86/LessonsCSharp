259af6e146f37c8cd07d941de919bb2c2f9120c0
guz86 <guzmo6@rambler.ru>
2021-07-16 14:59:35 +0300

    temperature

--- logicalOperators/Program.cs
+++ logicalOperators/Program.cs
@@ -7,15 +7,21 @@ namespace logicalOperators
         static void Main(string[] args)
         {
             //bool isInfected = true;
-            bool isHighTemperature = true;
-            bool isHeadPain = true;
+            int temperature = 36;
+            bool isHighTemperature = temperature>=37;
+            bool isHeadPain = false;
 
             //if (!isInfected)
             //{
             // Console.WriteLine("Вы здоровы!");
             // }
             if (isHighTemperature || isHeadPain) {
+                Console.WriteLine("Температура: "+ temperature);
                 Console.WriteLine("Возможно вы заболели!");
+            }
+            else
+            {
+                Console.WriteLine("Вы здоровы!");
             }
 
         }
