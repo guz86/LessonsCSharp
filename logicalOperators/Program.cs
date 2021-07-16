269cf77e2391c8baea0a6f63cc153c410e992a5f
guz86 <guzmo6@rambler.ru>
2021-07-16 14:41:14 +0300

    logicalOperators

---
+++ logicalOperators/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace logicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool isInfected = true;
+            bool isHighTemperature = true;
+            bool isHeadPain = true;
+
+            //if (!isInfected)
+            //{
+            // Console.WriteLine("Вы здоровы!");
+            // }
+            if (isHighTemperature && isHeadPain) {
+                Console.WriteLine("Возможно вы заболели!");
+            }
+
+        }
+    }
+}
