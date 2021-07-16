cfa61d324707ddb7889c80a83aa5eebc05ee693e
guz86 <guzmo6@rambler.ru>
2021-07-16 17:36:46 +0300

    while

--- calculatorIfElse/Program.cs
+++ calculatorIfElse/Program.cs
@@ -6,39 +6,55 @@ namespace calculatorIfElse
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите первое число: ");
-            double a = Convert.ToDouble ( Console.ReadLine());
-            Console.Write("Введите знак: * / % + - ");
-            char symbol = Convert.ToChar( Console.ReadLine());
-            Console.Write("Введите второе число: ");
-            double b = Convert.ToDouble (Console.ReadLine());
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Введите первое число: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите знак: * / % + - ");
+                char symbol = Convert.ToChar(Console.ReadLine());
+                Console.Write("Введите второе число: ");
+                double b = Convert.ToDouble(Console.ReadLine());
 
-            double result = 0;
+                double result = 0;
 
-            Console.WriteLine("Вычисляем: " + a + symbol + b);
+                Console.WriteLine("Вычисляем: " + a + symbol + b);
 
-            if (symbol == '*')
-            {
-                result = a * b;
-            }
-            else if (symbol == '/')
-            {
-                result = a / b;
-            }
-            else if (symbol == '%')
-            {
-                result = a % b;
-            }
-            else if (symbol == '+')
-            {
-                result = a + b;
-            }
-            else if (symbol == '-')
-            {
-                result = a - b;
-            }
+                if (symbol == '*')
+                {
+                    result = a * b;
+                }
+                else if (symbol == '/' && b != 0)
+                {
+                    result = a / b;
+                }
+                else if (symbol == '/' && b == 0)
+                {
+                    Console.WriteLine("На 0 делить нельзя.");
 
-            Console.WriteLine("Результат: "+ result);
+                }
+                else if (symbol == '%')
+                {
+                    result = a % b;
+                }
+                else if (symbol == '+')
+                {
+                    result = a + b;
+                }
+                else if (symbol == '-')
+                {
+                    result = a - b;
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестное действие");
+                }
+
+                Console.WriteLine("Результат: " + result);
+
+                Console.ReadKey();
+            }
+            
         }
     }
 }
