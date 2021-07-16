121ea6f9b4dcab9b31752bdff97307e8d637a4c4
guz86 <guzmo6@rambler.ru>
2021-07-16 16:03:19 +0300

    var1

---
+++ calculatorIfElse/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace calculatorIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double a = Convert.ToDouble ( Console.ReadLine());
+            Console.Write("Введите знак: * / % + - ");
+            char symbol = Convert.ToChar( Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double b = Convert.ToDouble (Console.ReadLine());
+
+            double result = 0;
+
+            Console.WriteLine("Вычисляем: " + a + symbol + b);
+
+            if (symbol == '*')
+            {
+                result = a * b;
+            }
+            else if (symbol == '/')
+            {
+                result = a / b;
+            }
+            else if (symbol == '%')
+            {
+                result = a % b;
+            }
+            else if (symbol == '+')
+            {
+                result = a + b;
+            }
+            else if (symbol == '-')
+            {
+                result = a - b;
+            }
+
+            Console.WriteLine("Результат: "+ result);
+        }
+    }
+}
