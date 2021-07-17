b1b1781ac7313ad04dd1ec32cb4adab054e60e76
guz86 <guzmo6@rambler.ru>
2021-07-17 15:20:02 +0300

    ver1

---
+++ theSumAndNumberOfEvenAndOddNumbers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace theSumAndNumberOfEvenAndOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startDigit;
+            int endDigit;
+
+            int evenCount = 0;
+            int oddCount = 0;
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            Console.Write("Введите с какого числа будем считать: ");
+            startDigit = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите до какого числа будем считать: ");
+            endDigit =  Convert.ToInt32(Console.ReadLine());
+            
+            
+            while (startDigit <= endDigit)
+            {
+                if (startDigit % 2 == 1)
+                {
+                    //Console.WriteLine("Нечетное число "+ startDigit);
+                    oddCount += 1;
+                    oddSum += startDigit;
+                }
+                else
+                {
+                    //Console.WriteLine("Четное число "+ startDigit);
+                    evenCount += 1;
+                    evenSum += startDigit;
+                }
+                
+
+                startDigit++;
+
+                
+            }
+             
+            Console.WriteLine("Количество четных: "+ evenCount);
+            Console.WriteLine("Количество нечетных: " + oddCount);
+
+            Console.WriteLine("Сумма четных: " + evenSum);
+            Console.WriteLine("Сумма нечетных: " + oddSum);
+        }
+    }
+}
