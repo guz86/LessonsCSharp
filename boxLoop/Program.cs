51672eb65fbcf4fe06f7cfc02379a8236ff950c5
guz86 <guzmo6@rambler.ru>
2021-07-18 11:53:59 +0300

    Triangle

--- boxLoop/Program.cs
+++ boxLoop/Program.cs
@@ -6,18 +6,46 @@ namespace boxLoop
     {
         static void Main(string[] args)
         {
-            int height = Convert.ToInt32(Console.ReadLine());
-            int width = Convert.ToInt32(Console.ReadLine());
+            int height = 10; // Convert.ToInt32(Console.ReadLine());
+            int width = 10; // Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < height; i++)
             {
+
+                
                 for (int j = 0; j < width; j++)
                 {
-                    Console.Write("[]");
+                   
+                        Console.Write("[]");
+                    
 
                 }
 
                 Console.WriteLine();
             }
+
+            Console.WriteLine(); 
+
+
+            int heightTriangle = 10; // Convert.ToInt32(Console.ReadLine());
+            //int widthTriangle = 20; // Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < heightTriangle; i++)
+            {
+
+               
+
+                for (int j = heightTriangle; j > i; j--)
+                {
+                    Console.Write("[]");
+                }
+
+
+                Console.WriteLine();
+            }
+            
+
+
+
         }
     }
 }
+
