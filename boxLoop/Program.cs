df334e063d658a8aae667714b7a6f2dc6e506287
guz86 <guzmo6@rambler.ru>
2021-07-18 14:48:51 +0300

    heightTriangle7

--- boxLoop/Program.cs
+++ boxLoop/Program.cs
@@ -93,16 +93,44 @@ namespace boxLoop
 
             for (int i = 0; i < heightTriangle6; i++)
             {
-                Console.SetCursorPosition(heightTriangle6 - i, i+100);
+                Console.SetCursorPosition(heightTriangle6 - i, i+50);
 
                 for (int j = 0; j <= i; j++)
                 {
                     Console.Write("^");
                 }
 
+                
+
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine();
+
+
+            int heightTriangle7 = 10; // Convert.ToInt32(Console.ReadLine()); 
+            for (int i = 0; i < heightTriangle7; i++)
+            {
+
+
+
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = heightTriangle7; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+
                 Console.WriteLine();
 
             }
+
+            Console.WriteLine();
+
 
 
         }
