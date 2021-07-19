85bb5202daa84707bdc1e14f081552285e6b6d2c
guz86 <guzmo6@rambler.ru>
2021-07-19 16:43:19 +0300

    inputDate

---
+++ ternaryOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ternaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            // унарная операция - операция над одним объектом (операндом)
+            a++;
+            // бинарная операция
+            int b = 0;
+            int c = a + b;
+            // тернарный оператор
+            // первый операнд это условие ? второй операнд - если истина : третий операнд если ложь
+            // конструкция if else только в более компактном виде для простых операций
+            bool check = a == 1 ?  true  :  false;
+            Console.WriteLine(check);
+
+
+            // если вводим отрицательное то его обнуляем, если положительное оставляем
+            int inputDate = Convert.ToInt32(Console.ReadLine());
+            int outputDate = inputDate > 0 ? inputDate : 0;
+            Console.WriteLine(outputDate);
+
+        }
+    }
+}
