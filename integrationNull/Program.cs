4f93f298d016cd20723f8233469bfd6a558f74ad
guz86 <guzmo6@rambler.ru>
2021-08-29 13:33:51 +0300

    string.Empty

--- integrationNull/Program.cs
+++ integrationNull/Program.cs
@@ -11,13 +11,28 @@ namespace integrationNull
 
             if (str == null)
             {
-                result = "нет данных";
+                result = "нет данных в str";
             }
             else
             {
                 result = str;
             }
             Console.WriteLine(result);
+
+            // unhandled exception необработанное исключение
+            //Console.WriteLine( "количество символов в str " + str.Length);
+            // string resultNew = str ?? "";
+            string resultNew = str ?? string.Empty;
+            Console.WriteLine("количество символов в str " + resultNew.Length);
+
+
+
+
+            // easy
+            string strNew = null;
+            Console.WriteLine(strNew ?? "нет данных в strNew");
+
+
         }
     }
 }
