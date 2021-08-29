42304be4efad301bcfba4f84b8cdeef48e66e814
guz86 <guzmo6@rambler.ru>
2021-08-29 13:15:38 +0300

    init

---
+++ integrationNull/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace integrationNull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            string result;
+
+            if (str == null)
+            {
+                result = "нет данных";
+            }
+            else
+            {
+                result = str;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
