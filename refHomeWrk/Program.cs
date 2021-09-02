aec4b68db2301ab457b5cbb821efb50318d3410b
guz86 <guzmo6@rambler.ru>
2021-09-02 21:45:57 +0300

    ver2

--- refHomeWrk/Program.cs
+++ refHomeWrk/Program.cs
@@ -6,17 +6,37 @@ namespace refHomeWrk
     {
         static void Main(string[] args)
         {
-            int[] myArray = new int[5];
-            int element = 10;
-            Resize(ref myArray, element);
+            // int[] myArray = new int[5];
+            int[] myArray = { 1, 2, 3, 66 };
+            int element = 2;
+            myArray = Resize(ref myArray, element);
             int length = myArray.Length;
         }
 
         //Написать метод Resize изменяющий количество элементов массива(метод должен иметь возможность увеличить или уменьшить количество элментов массива)
-        static void Resize(ref int[] arr, int element)
+        static int[] Resize(ref int[] arr, int element)
         {
-            arr = new int[element];
+            int [] temparr = new int[element];
+            int lengthArray = 0;
+            if (element < arr.Length)
+            {
+                lengthArray = temparr.Length;
+            }
+            else
+            {
+                lengthArray = arr.Length;
+            }
+
+            for (int i = 0; i < lengthArray; i++)
+            {
+                  temparr[i] = arr[i];
+            }
+            return temparr;
         }
+
+        // array.Resize
+
+
 
 
         //Написать методы для добавления элемента в начало массива, в конец массива и по указанному индексу
