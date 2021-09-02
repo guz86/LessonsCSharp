cb7baeee7b42771207f834e350e466c4a03f5a8a
guz86 <guzmo6@rambler.ru>
2021-09-03 01:49:10 +0300

    ver2

--- refHomeWrk/Program.cs
+++ refHomeWrk/Program.cs
@@ -8,7 +8,7 @@ namespace refHomeWrk
         {
             // int[] myArray = new int[5];
             int[] myArray = { 1, 2, 3, 66 };
-            int element = 2;
+            int element = 12;
             MyResize(ref myArray, element);
             int length = myArray.Length;
 
@@ -19,12 +19,16 @@ namespace refHomeWrk
             Resize(ref Array2, element);
             int length2 = Array2.Length;
 
-
-
             int elementValue = 55;
             AddElementBegin(ref myArray, elementValue);
             int length3 = myArray.Length;
 
+
+            int[] myArray2 = { 1, 1, 1, 2, 3, 66 };
+            int elementValue2 = 35;
+            int elementIndex = myArray2.Length;
+            AddElementIndex(ref myArray2, elementIndex, elementValue2);
+            int length4 = myArray2.Length;
 
 
         }
@@ -61,12 +65,28 @@ namespace refHomeWrk
         }
 
 
-        //Написать методы для добавления элемента по указанному индексу
+        //Написать методы для удаления первого элемента массива, последнего элемента массива и элемента по указанному индексу.
 
+        // 1 Написать методы для добавления элемента по указанному индексу
+        static void AddElementIndex(ref int[] arr, int index, int value)
+        {     
+            int[] tempArray = new int[arr.Length + 1];
+            for (int i = 0, j =0 ; i < arr.Length + 1;  i++, j++)
+            {
+                if (i == index)
+                {
+                    tempArray[index] = value;
+                    j--;
+                }
+                else
+                {
+                     tempArray[i] = arr[j];
+                }
+            }
+            arr = tempArray;
+        }
 
-
-
-        //Написать методы для добавления элемента в начало массива
+        // 2 Написать методы для добавления элемента в начало массива
         static void AddElementBegin(ref int[] arr, int elementValue)
         {
             int[] tempArray = new int[arr.Length+1];
@@ -79,9 +99,7 @@ namespace refHomeWrk
             arr = tempArray;
         }
 
-
-
-        //Написать методы для удаления первого элемента массива, последнего элемента массива и элемента по указанному индексу.
+        
 
 
 
