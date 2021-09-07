7ac4c5f77d5d4796cb796499fd892e052e744ce3
Pavel Gordienko <guzmo6@rambler.ru>
2021-09-07 23:15:14 +0300

    Start Stop Restart

--- STOPWATCH/Program.cs
+++ STOPWATCH/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace STOPWATCH
 {
     class Program
     {
+        // измерить время выполнения кода
         static void Main(string[] args)
         {
+            // создаем объект класса Stopwatch
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            for (int i = 0; i < 5000;i++)
+            {
+                Console.WriteLine("");
+            }
+
+            sw.Stop();
+
+            Console.WriteLine($"Time: {sw.ElapsedMilliseconds}");
+
             
+            var sm = new Stopwatch();
+            sm.Start();
+            System.Threading.Thread.Sleep(100);
+            sm.Stop();
+            Console.WriteLine($"Time: {sm.ElapsedMilliseconds}");
+
+
+            sm.Restart();
+            System.Threading.Thread.Sleep(500);
+            sm.Stop();
+            Console.WriteLine($"Time: {sm.ElapsedMilliseconds}");
         }
     }
 }
