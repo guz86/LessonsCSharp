1ca1aa47041ff67912984779e27aa9ba03ddc04f
Pavel Gordienko <guzmo6@rambler.ru>
2021-09-08 18:49:53 +0300

    Foo

--- ParamsCSharp/Program.cs
+++ ParamsCSharp/Program.cs
@@ -25,7 +25,18 @@ namespace ParamsCSharp
             }
             return result;
         }
-        // params указывается последним static int Sum(string str, params int[] parameters)
+        // params указывается последним static int Sum(string str, params int[] parameters)   // int result = Sum("test", 3, 4, 4, 3, 2, 54);
+
+        // пример с разными типами данных
+        static void Foo(params object[] parameters)
+        {
+            string message = "Тип данных: {0}, значение: {1}";
+
+            foreach (var item in parameters)
+            {
+                Console.WriteLine(message, item.GetType(), item);
+            }
+        }
 
 
         static void Main(string[] args)
@@ -42,6 +53,7 @@ namespace ParamsCSharp
             Console.WriteLine(result3);
             Console.WriteLine(result99);
             Console.WriteLine(result0);
+            Foo("stroka",'$',332,33.33f, true);
         }
     }
 }
