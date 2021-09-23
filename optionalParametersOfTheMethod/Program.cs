55383fe4953a72c47b5cffb196b87a1d5dc03689
Pavel Gordienko <guzmo6@rambler.ru>
2021-09-23 15:56:52 +0300

    parametr name

--- optionalParametersOfTheMethod/Program.cs
+++ optionalParametersOfTheMethod/Program.cs
@@ -7,14 +7,15 @@ namespace optionalParametersOfTheMethod
         static void Main(string[] args)
         {
             //int result = Sum(2, 4);
-            int result = Sum(2, 4, true);
+            //int result = Sum(2, 4, true);
+            int result = Sum(2, 4, enableLogging: true);
             Console.WriteLine(result);
         }
-        static int Sum(int a, int b, bool Logging = false)
+        static int Sum(int a, int b, bool enableLogging = false)
         {
             int result = a + b;
 
-            if (Logging)
+            if (enableLogging)
             {
                 Console.WriteLine($"Сумма двух чисел {a} и {b} равна {result}");
             }
