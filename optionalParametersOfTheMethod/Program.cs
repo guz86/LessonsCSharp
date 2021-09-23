d747da75852a5a9ebfc9f8b2b56cb78b32813162
Pavel Gordienko <guzmo6@rambler.ru>
2021-09-23 15:30:12 +0300

    optionalParametersOfTheMethod

---
+++ optionalParametersOfTheMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace optionalParametersOfTheMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int result = Sum(2, 4);
+            int result = Sum(2, 4, true);
+            Console.WriteLine(result);
+        }
+        static int Sum(int a, int b, bool Logging = false)
+        {
+            int result = a + b;
+
+            if (Logging)
+            {
+                Console.WriteLine($"Сумма двух чисел {a} и {b} равна {result}");
+            }
+
+            return result;
+        }
+    }
+}
