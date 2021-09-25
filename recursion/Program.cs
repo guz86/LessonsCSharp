7c1e88b8a180d08edd2970d5f3676dc1298bf5a5
Pavel Gordienko <guzmo6@rambler.ru>
2021-09-25 07:15:54 +0300

    init

---
+++ recursion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace recursion
+{
+    class Program
+    {
+        static void Foo()
+        {
+            Console.WriteLine("Foo");
+        }
+        static void Main(string[] args)
+        {
+            Foo();
+        }
+    }
+}
