c98a3d6ff6d03b13d595327ce937b4e98e29b1cd
Pavel Gordienko <guzmo6@rambler.ru>
2021-10-07 11:38:54 +0300

    ver1

---
+++ dzArrayRecursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace dzArrayRecursion
+{
+    class Program
+    {
+        // вывод массива с помощью рекурсии
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 1, 4, 5, 6, 7, 8 };
+
+            Output(myArray,0);
+            
+        }
+
+        static void Output(int [] array, int i)
+        {
+            Console.WriteLine(array[i]);
+            if (i >= array.Length-1)
+            {
+                return;
+            }
+            i++;
+            Output(array, i);
+        }
+    }
+}
