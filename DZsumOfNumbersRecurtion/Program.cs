55de7c4ca8c050d8a5a42616c308b5dc83f65aab
Pavel Gordienko <guzmo6@rambler.ru>
2021-10-10 01:19:01 +0300

    SumOfNumbersV1

---
+++ DZsumOfNumbersRecurtion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DZsumOfNumbersRecurtion
+{
+    class Program
+    {
+        // найти сумму цифр числа с помощью рекурсии
+        // find the sum of the numbers by recursion
+        // 561  => 12
+        static void Main(string[] args)
+        {
+            int numbers = 561;
+            SumOfNumbersV1(numbers);
+        }
+        static void SumOfNumbersV1(int numbers)
+        {
+            string Str = Convert.ToString(numbers);
+
+            int sum = 0;
+
+            for (int i = 0; i < Str.Length; i++)
+            {
+                sum += int.Parse(Str[i].ToString());
+            }
+            Console.WriteLine(sum);
+        }
+
+
+
+    }
+}
