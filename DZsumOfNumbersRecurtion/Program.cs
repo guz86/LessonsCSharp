13c5ec87c03abd78287aec4ed7cc839bb4505a28
Pavel Gordienko <guzmo6@rambler.ru>
2021-10-10 01:46:05 +0300

    V2 V3

--- DZsumOfNumbersRecurtion/Program.cs
+++ DZsumOfNumbersRecurtion/Program.cs
@@ -10,7 +10,12 @@ namespace DZsumOfNumbersRecurtion
         static void Main(string[] args)
         {
             int numbers = 561;
+            string numbers2 = "561";
             SumOfNumbersV1(numbers);
+            SumOfNumbersV2(numbers);
+            Console.WriteLine(SumOfNumbersV3(numbers2));
+            SumOfNumbersLesson(numbers);
+
         }
         static void SumOfNumbersV1(int numbers)
         {
@@ -25,7 +30,31 @@ namespace DZsumOfNumbersRecurtion
             Console.WriteLine(sum);
         }
 
+        static void SumOfNumbersV2(int numbers, int sum = 0, int i = 0)
+        {
 
+            string Str = Convert.ToString(numbers);
+
+            if (i < Str.Length)
+            {
+                sum += int.Parse(Str[i].ToString());
+                i++;
+                SumOfNumbersV2(numbers, sum, i);
+            }
+            else
+            {
+                Console.WriteLine(sum);
+            }
+            
+
+
+        }
+ 
+        static int SumOfNumbersV3(string num) => num.Length > 0 ? Convert.ToInt32(num[0] - '0') + SumOfNumbersV3(num[1..]) : 0;
+        static void SumOfNumbersLesson(int numbers)
+        {
+
+        }
 
     }
 }
