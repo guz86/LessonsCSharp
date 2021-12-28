5c5c4de64546b3f2f57ddb671f52e3c4ccd9edb1
Pavel Gordienko <guzmo6@rambler.ru>
2021-12-29 02:43:32 +0300

    constructor

--- dickoop/Dick.cs
+++ dickoop/Dick.cs
@@ -8,5 +8,17 @@ namespace dickoop
 {
     class Dick
     {
+        private float _length;
+        private float _radius;
+
+        //private float _radius = 3f;
+
+        public Dick(float length, float radius)
+        {
+            _length = length;
+            _radius = radius;
+            //_radius = 3f;
+
+        }
     }
 }
