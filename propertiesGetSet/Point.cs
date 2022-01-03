011753d2d57288833f736d4bcf040a8ff1f0b49e
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-03 06:14:43 +0300

    class Point

---
+++ propertiesGetSet/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propertiesGetSet
+{
+    class Point
+    {
+        private int _x;
+
+        public int GetX()
+        {
+            return _x;
+
+        }
+
+        public void  SetX(int x)
+        {
+            _x = x;
+        }
+    }
+}
--- propertiesGetSet/Program.cs
+++ propertiesGetSet/Program.cs
@@ -6,7 +6,10 @@ namespace propertiesGetSet
     {
         static void Main(string[] args)
         {
-            
+            Point point = new Point();
+
+            point.GetX();
+            point.SetX(10);  
         }
     }
 }
