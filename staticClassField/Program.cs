89bed6e6de2271cd5876a00858e6e3df04171746
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-05 03:27:31 +0300

    init

---
+++ staticClassField/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace staticClassField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassWithStaticField.b = 6;
+
+            ClassWithStaticField newClass1 = new ClassWithStaticField();
+            newClass1.a = 5;
+
+            var newClass2 = new ClassWithStaticField();
+            newClass2.a = 4;
+
+
+
+
+        }
+    }
+}
