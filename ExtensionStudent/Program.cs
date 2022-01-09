db184ddf5170a15a64bde4cbbd6a23f1558d6c3c
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-09 06:56:10 +0300

    sealed class Student

---
+++ ExtensionStudent/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    sealed class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
