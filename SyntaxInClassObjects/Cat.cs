8a4b63044799b57e13481a0b276d3baea5779ae6
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-11 13:37:57 +0300

    init

---
+++ SyntaxInClassObjects/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyntaxInClassObjects
+{
+    class Cat
+    {
+        // синтаксис инициализации объектов класса
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Cat()
+        {
+
+        }
+
+    }
+}
---
+++ SyntaxInClassObjects/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxInClassObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+
+            cat.Age = 5;
+            cat.Name = "Boris";
+
+        }
+    }
+}
