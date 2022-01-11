d39498b4928a0b6ebf85739d2649840fc5f8f2d4
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-11 08:32:33 +0300

    _myLogger

--- ConstReadonly/Car.cs
+++ ConstReadonly/Car.cs
@@ -8,6 +8,18 @@ namespace ConstReadonly
 {
     class Car
     {
+        private const string ENGINE_IS_START_MESSAGE = "Двигатель запущен";
+        private const string ATTEMPT_TO_START_ENGINE_MESSAGE = "Завожу двигатель";
+        private const string ENGINE_IS_ALREADY_STARTED_MESSAGE = "Двигатель уже запущен!";
+        private const string ATTEMPT_TO_DRIVE_MESSAGE = "Педаль газа в пол";
+        private const string DRIVE_MESSAGE = "Машина поехала";
+        private const string DRIVE_ERROR_MESSAGE = "Заведи двигатель!";
+
+        // некий сервис который будет использовать Car под свои нужды
+        private readonly MyLogger _myLogger;
+
+
+
         public void StartEngine()
         {
 
