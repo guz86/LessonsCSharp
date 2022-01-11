e3aa35d532ed0da928bc5a8dc3a392e19ae54ce4
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-11 08:10:12 +0300

    MyLogger

---
+++ ConstReadonly/MyLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstReadonly
+{
+    class MyLogger
+    {
+        public void Info (string message)
+        {
+            Console.WriteLine($"{DateTime.Now}\t|INFO|\t{message}");
+        }
+    }
+}
