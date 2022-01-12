80131a7a27041153d78377e22b74aae54877ba5f
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-12 13:45:57 +0300

    Student

---
+++ Inheritance2/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    class Student : Person
+    {
+        public void Learn()
+        {
+            Console.WriteLine("Учится");
+        }
+    }
+}
