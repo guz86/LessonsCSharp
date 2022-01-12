e99ff121293d81032f2d3768a6265671de3948dc
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-12 13:43:54 +0300

    Security

---
+++ Inheritance2/Security.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    class Security : Person
+    {
+        public void Guard()
+        {
+            Console.WriteLine("Охраняю");
+        }
+    }
+}
