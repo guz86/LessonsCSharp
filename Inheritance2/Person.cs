13e3ba074c91bf5da02538afc547833ae2f1a28e
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-12 13:41:26 +0300

    init

---
+++ Inheritance2/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine($"Имя: {FirstName}\t Фамилия: {LastName}");
+        }
+
+    }
+}
---
+++ Inheritance2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Inheritance2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person { FirstName = "Danil", LastName = "Bodrov" };
+            person1.PrintFullName();
+        }
+    }
+}
