38a5cf30d419b543e4a506c721cea3149c76ae26
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-12 14:13:09 +0300

    PrintPersons(people);

--- Inheritance2/Program.cs
+++ Inheritance2/Program.cs
@@ -6,8 +6,24 @@ namespace Inheritance2
     {
         static void Main(string[] args)
         {
-            Person person1 = new Person { FirstName = "Danil", LastName = "Bodrov" };
-            person1.PrintFullName();
+            //Person person1 = new Person { FirstName = "Danil", LastName = "Bodrov" };
+            //person1.PrintFullName();
+
+            Teacher teacher1 = new Teacher { FirstName = "Evgen", LastName = "Petrov" };
+            Student student1 = new Student { FirstName = "Sergey", LastName = "Alexeev" };
+
+            Person[] people = { teacher1, student1 };
+
+            PrintPersons(people);
+
+
+            static void PrintPersons(Person[] people)
+            {
+                foreach (var person in people)
+                {
+                    person.PrintFullName();
+                }
+            }
 
             // add new item class diogram
         }
