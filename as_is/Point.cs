815a08c3d9ca53653ad935d8728b5a45ba25219b
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-14 10:43:00 +0300

    class Point

---
+++ as_is/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace as_is
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"X\t{X}");
+            Console.WriteLine($"Y\t{Y}");
+        }
+    }
+}
--- as_is/Program.cs
+++ as_is/Program.cs
@@ -6,7 +6,10 @@ namespace as_is
     {
         static void Main(string[] args)
         {
-            
+            Point point1 = new Point();
+            point1.X = 1;
+            point1.Y = 5;
+            point1.Print();
         }
     }
 }
