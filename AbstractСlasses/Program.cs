6ef7a6be0e21b6f99a8472c539050df7a61aebd0
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-21 04:55:33 +0300

    class Player

---
+++ AbstractСlasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractСlasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+        }
+    }
+}
