b14c213c0caf8a4755b78d9871b2379a619e30c3
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-21 16:07:24 +0300

    IHasInfo

--- InterfaceWeapon/Weapon.cs
+++ InterfaceWeapon/Weapon.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace InterfaceWeapon
 {
-    abstract class Weapon
+    abstract class Weapon : IHasInfo
     {
         public abstract int Damage { get; }
         public abstract void Fire();
