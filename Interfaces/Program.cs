89e261bc8c40d9e043e3abed8e1c12072906a237
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-21 15:55:53 +0300

    output

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -7,8 +7,11 @@ namespace Interfaces
         static void Main(string[] args)
         {
             IDataProcessor dataProcessor = new ConsoleDataProcessor();
-            
-            
+            dataProcessor.ProcessorData(new ApiDataProvider());
+            dataProcessor.ProcessorData(new DbDataProvider());
+            dataProcessor.ProcessorData(new FileDataProvider());
+
+
         }
     }
 }
