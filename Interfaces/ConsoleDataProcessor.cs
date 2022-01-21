d5ca6239255ef56c48e91403880a4485750b5c5a
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-21 15:33:37 +0300

    ConsoleDataProcessor

---
+++ Interfaces/ConsoleDataProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class ConsoleDataProcessor : IDataProcessor
+    {
+        public void ProcessorData(IDataProvider dataProvider)
+        {
+            Console.WriteLine(dataProvider.GetData());
+        }
+    }
+}
