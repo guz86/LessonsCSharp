e0dbd562fdf88baf8488f06fef5f0561f85d2852
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-22 23:30:53 +0300

    BenchTest

--- BenchTest/MyParser.cs
+++ BenchTest/MyParser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BenchmarkdotnetTest
+namespace BenchTest
 {
     class MyParser
     {
@@ -41,6 +41,4 @@ namespace BenchmarkdotnetTest
             return result;
         }
     }
-
-
 }
--- Benchmark/MyParserBenchmark.cs
+++ Benchmark/MyParserBenchmark.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
-using BenchmarkdotnetTest;
+
 
 namespace Benchmark
 {
     class MyParserBenchmark
     {
+        private const string STRING_TO_PARSE_WITH_ERROR = "sdfsdf3";
+        private const string STRING_TO_PARSE = "44";
 
+
+        //private readonly BenchmarkdotnetTest. myParser = new MyParser();
     }
 }
