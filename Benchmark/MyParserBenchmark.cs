39422a8c3e25b38477ad5e50d448c7eee707a779
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-22 23:04:25 +0300

    MyParserBenchmark

---
+++ Benchmark/MyParserBenchmark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmark
+{
+    class MyParserBenchmark
+    {
+    }
+}
