1defe3e6f466c89b594863863d424905b8a9c4c2
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-25 04:41:22 +0300

    init

---
+++ MultipleInheritanceOfInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultipleInheritanceOfInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // множественное наследование интерфейсов
+        }
+    }
+}
