4bfecc104ac26543d943f27c6c58954a87e0711b
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-25 05:39:19 +0300

    secondInterface

--- ExplicitIimplementationOfTheInterface/Program.cs
+++ ExplicitIimplementationOfTheInterface/Program.cs
@@ -10,11 +10,17 @@ namespace ExplicitIimplementationOfTheInterface
 
             //  может ли класс реализовать два интерфейса у которых объявлено 2 одинаковых метода
             MyClass myClass = new MyClass();
+            myClass.Action();
             Foo(myClass);
             Baa(myClass);
             MyOtherClass myOtherClass = new MyOtherClass();
+            //myOtherClass.Action(); - неоднозначность
             Foo(myOtherClass);
             Baa(myOtherClass);
+
+            // ссылка с типом интерфейса у которой класс который реализует данный интерфейс
+            ISecondInterface secondInterface = new MyOtherClass();
+            secondInterface.Action();
 
         }
 
