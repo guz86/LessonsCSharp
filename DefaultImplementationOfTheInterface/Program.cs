72b704b0dae16c1de5c7124b1dff53a4d6d05c91
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-25 05:48:14 +0300

    init

---
+++ DefaultImplementationOfTheInterface/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DefaultImplementationOfTheInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // реализация интерфейса по-умолчанию
+
+        }
+    }
+}
