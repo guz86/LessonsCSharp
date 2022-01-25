89cb512d6ffa1344c2bc6477b14441d015351a88
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-25 05:03:39 +0300

    init

---
+++ ExplicitIimplementationOfTheInterface/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExplicitIimplementationOfTheInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // явная реализация интерфейсов
+
+            //  может ли класс реализовать два интерфейса у которых объявлено 2 одинаковых метода
+
+        }
+    }
+}
