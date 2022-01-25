be2579f41bea85349346cec186d1adc0cf9ccf15
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-25 05:27:39 +0300

    MyClass : IFirstInterface, ISecondInterface

---
+++ ExplicitIimplementationOfTheInterface/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitIimplementationOfTheInterface
+{
+    class MyClass : IFirstInterface, ISecondInterface
+    {
+        public void Action()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
