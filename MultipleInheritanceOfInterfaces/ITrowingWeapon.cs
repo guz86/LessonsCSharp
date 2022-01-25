3640d9a6f3c9724a28160c7580cbf7e255845fd0
Pavel Gordienko <guzmo6@rambler.ru>
2022-01-25 04:55:52 +0300

    class Knife : ITrowingWeapon

---
+++ MultipleInheritanceOfInterfaces/ITrowingWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritanceOfInterfaces
+{
+    // добавляем через наследование метод Fire
+    interface ITrowingWeapon : IWeapon
+    {
+        void Trow();
+    }
+}
---
+++ MultipleInheritanceOfInterfaces/Knife.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritanceOfInterfaces
+{
+    class Knife : ITrowingWeapon
+    {
+        public void Fire()
+        {
+            Console.WriteLine($"{GetType().Name} - Хыщь");
+        }
+
+        public void Trow()
+        {
+            Console.WriteLine($"{GetType().Name} - Фьють");
+        }
+    }
+}
