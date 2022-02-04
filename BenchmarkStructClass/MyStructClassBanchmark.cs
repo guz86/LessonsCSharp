a774afb8c88e087d263ca6dd52f07c9a6edde29c
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-04 23:19:26 +0300

    BenchmarkStructClass

---
+++ BenchmarkStructClass/MyStructClassBanchmark.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using StructureXY;
+
+namespace BenchmarkStructClass
+
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class MyStructClassBanchmark
+    {
+        [Benchmark]
+        public void StructTest()
+        {
+            StructPoint structPoint = new StructPoint();
+            structPoint.X = 1;
+            structPoint.Y = 2;
+            var result = structPoint.X + structPoint.Y;
+        }
+
+        [Benchmark]
+        public void ClassTest()
+        {
+            ClassPoint classPoint = new ClassPoint();
+            classPoint.X = 1;
+            classPoint.Y = 2;
+            var result = classPoint.X + classPoint.Y;
+        }
+
+    }
+}
---
+++ BenchmarkStructClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkStructClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MyStructClassBanchmark>();
+        }
+    }
+}
