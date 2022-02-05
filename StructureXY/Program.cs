105819f7114661579f29d8cea8660b9794ff9d62
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-05 17:23:04 +0300

    classesAreEqual structsAreEqual

--- StructureXY/Program.cs
+++ StructureXY/Program.cs
@@ -27,6 +27,23 @@ namespace StructureXY
             // (при передаче структуры поля копируются в новый экземпляр)
             Bar(structPoint);
 
+
+            // Equals
+
+            ClassPoint classPoint1 = new ClassPoint();
+            ClassPoint classPoint2 = new ClassPoint();
+
+            // (сравнивает ссылки ) две разные ссылки на разные объекты в куче
+            bool classesAreEqual = classPoint1.Equals(classPoint2); // false
+
+            StructPoint structPoint1 = new StructPoint();
+            StructPoint structPoint2 = new StructPoint();
+
+            // сравнение идет по значениям полей
+            bool structsAreEqual = structPoint1.Equals(structPoint2); // true
+
+
+
         }
     }
 }
