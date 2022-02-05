9dec2c1e95d9ecf170e7b520a9901594fa961d04
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-05 14:59:51 +0300

    Foo(classPoint); Bar(structPoint);

--- StructureXY/Program.cs
+++ StructureXY/Program.cs
@@ -7,6 +7,25 @@ namespace StructureXY
         static void Main(string[] args)
         {
             // две реализации Point
+            static void Foo(ClassPoint classPoint)
+            {
+                classPoint.X++;
+                classPoint.Y++;
+            }
+            static void Bar(StructPoint structPoint)
+            {
+                structPoint.X++;
+                structPoint.Y++;
+            }
+
+            ClassPoint classPoint = new ClassPoint();
+            StructPoint structPoint = new StructPoint();
+
+            // передается ссылка на одни и те же данные в управляемой куче и реальные данные поменяются
+            Foo(classPoint);
+            // структуры передаются по значению, при выходе из метода поля 0
+            // (при передаче структуры поля копируются в новый экземпляр)
+            Bar(structPoint);
 
         }
     }
