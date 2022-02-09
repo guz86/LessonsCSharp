3a49f2f10e17a2bbe2fd4f82532f60c778e4b743
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-09 17:57:41 +0300

    Swap(ref a, ref b);

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -6,7 +6,20 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            
+            // обобщения, код будет одинаково работать с разными типами данных
+            // избежать процессы упаковки и распаковки
+
+            int a = 1, b = 2;
+            Console.WriteLine($"a = {a}, b = {b}");
+
+            Swap(ref a, ref b); 
+            Console.WriteLine($"a = {a}, b = {b}");
+        }
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp; 
         }
     }
 }
