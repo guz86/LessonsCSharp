1b1b841c55601db4856c672e1268f317fa022331
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-11 04:39:28 +0300

    init

---
+++ Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // информация о нажатой клавише на клавиатуре
+                ConsoleKey consoleKey = Console.ReadKey().Key;
+                Console.WriteLine(consoleKey);
+                if (consoleKey == ConsoleKey.Enter)
+                {
+                    Console.WriteLine($"Нажата клавиша {ConsoleKey.Enter}");
+                }
+
+            }
+        }
+    }
+}
