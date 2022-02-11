4f8ff1999ac34ab8ca0e2ddcbcffe19ab7c86b6e
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-11 03:06:05 +0300

    human4 is Boozer boozer5

--- ExtremeCode12/Program.cs
+++ ExtremeCode12/Program.cs
@@ -11,6 +11,41 @@ namespace ExtremeCode12
             Boozer boozer = new Boozer();
             boozer.SayHello();
             boozer.SayBoo();
+
+            //upcast
+            Human human1 = boozer;
+            //human1.SayBoo();
+            //downcast
+            Boozer boozer1 = (Boozer)human1;
+            boozer1.SayBoo();
+
+            //as is
+            Boozer boozer2 = new Boozer();
+            Human human2 = boozer2;
+            bool boolBoozer = human2 is Boozer;
+            if (boolBoozer)
+            {
+                boozer2 = (Boozer)human2;
+            }
+
+            // может быть null
+            Boozer boozer3 = new Boozer();
+            Human human3 = boozer3;
+
+            boozer3 = human3 as Boozer;
+            if (boozer3 != null)
+            {
+                boozer2.SayBoo();
+            }
+
+            //с#7
+            Boozer boozer4 = new Boozer();
+            Human human4 = boozer4;
+
+            if (human4 is Boozer boozer5)
+            {
+                boozer5.SayBoo();
+            }
         }
     }
 }
