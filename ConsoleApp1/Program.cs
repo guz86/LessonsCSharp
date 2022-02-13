c987368948382a6ae73134294fe799b753c5a00e
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-13 19:07:05 +0300

    using Myproj;

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Myproj;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = MyLibrory.Add(1, 3);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyLibrory/MyLibrory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Myproj
+{
+    public static class MyLibrory
+    {
+        // DLL
+
+        //методы
+        public static double Add(double firstValue, double secondValue)
+        {
+            return firstValue + secondValue;
+        }
+        public static double Multiply(double firstValue, double secondValue)
+        {
+            return firstValue * secondValue;
+        }
+        public static double Divide(double firstValue, double secondValue)
+        {
+            if (secondValue == 0)
+            {
+                return 0;
+            }
+            return firstValue / secondValue;
+        }
+    }
+}
