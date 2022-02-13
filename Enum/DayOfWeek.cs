9e603eb53256211a16c1aebe90e65bedd017d6d5
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-13 16:12:46 +0300

    Enum.IsDefined

---
+++ Enum/DayOfWeek.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumSimple
+{
+    enum DayOfWeek : byte
+    {
+        // диапозон от 1 до 7
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+}
--- Enum/Program.cs
+++ Enum/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Enum
+namespace EnumSimple
 {
     class Program
     {
@@ -17,6 +17,48 @@ namespace Enum
                     Console.WriteLine($"Нажата клавиша {ConsoleKey.Enter}");
                 }
 
+
+                // дни недели
+                DayOfWeek dayOfWeek = DayOfWeek.Monday;
+                // узнаем тип данных
+                //Type enumType = dayOfWeek.GetType();
+                Type enumType = typeof(DayOfWeek);
+                Console.WriteLine(Enum.GetUnderlyingType(enumType));
+
+                //следующий день
+                Console.WriteLine(GetNextDay(DayOfWeek.Friday));
+
+
+
+
+                // к какому числу соотвествует день недели
+                Console.WriteLine((DayOfWeek)3); // число 3 приводим к нашему enum
+
+                // что делать если число выходит за пределы enum, пришло извне
+                int day = 44;
+                //Console.WriteLine((DayOfWeek)day);
+                // тогда вернется просто число из int
+                // проверка элемента в соотвествующем enum DayOfWeek
+                if (Enum.IsDefined(typeof(DayOfWeek), (byte)day))
+                {
+                    dayOfWeek = (DayOfWeek)day;
+                }
+                else
+                {
+                    throw new Exception("Invalid DayofWeek day");
+                }
+
+                Console.WriteLine(dayOfWeek);
+            }
+
+            static DayOfWeek GetNextDay(DayOfWeek day)
+            {
+                if (day < DayOfWeek.Sunday)
+                {
+                   return day + 1;
+                }
+
+                return DayOfWeek.Monday;
             }
         }
     }
