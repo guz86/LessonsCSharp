bddda4e35bc4d5efdd4f9fed48cf4d176f234335
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-16 02:57:05 +0300

    class Neuron

---
+++ Neyron/Neuron.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neyron
+{
+    public class Neuron
+    {
+        private decimal weight = 0.5m;
+        public decimal LastError { get; private set; }
+
+        // добавим сглаживание, чтобы шаг коррекции не было очень большим
+        // чем больше, тем хуже точность, но выше скорость обучения
+        public decimal Smoothing { get; set; } = 0.000001m;
+
+        // метод который будет получать входящий сигнал извне и будет его обрабатывать
+        public decimal ProcessingInputData(decimal input)
+        {
+            return input * weight;
+        }
+
+        // инвертировать действие
+        public decimal RestoreInputData(decimal input)
+        {
+            return input / weight;
+        }
+
+        // метод который будет обучать наш нейрон
+        // нужно показать пример как должно быть
+        public void Train(decimal input, decimal expectedResult)
+        {
+            // как должно быть
+            var ActualResult = weight * input;
+            // вычисляем ошибку
+            LastError =  expectedResult - ActualResult;
+            // вычислим значение на которое нам нужно скорректировать
+            // в weght чтобы эту ошибку исправить
+            //var correction = LastError / ActualResult;
+            var correction = (LastError / ActualResult) * Smoothing;
+            // прибавляем его к весу
+            weight += correction;
+        }
+    }
+    
+}
--- Neyron/Program.cs
+++ Neyron/Program.cs
@@ -6,7 +6,41 @@ namespace Neyron
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // километры в мили
+            decimal km = 100;
+            decimal miles = 62.1371m;
+
+            // при передаче нейрону km он должен их конвертировать в правильное количество миль
+
+            Neuron neuron = new Neuron();
+            Console.WriteLine($"{neuron.ProcessingInputData(km)}  миль в {km} километрах");
+            // обучим нейрон выдавать правильный результат
+            int i = 0;
+            do
+            {
+                i++;
+                // обучайся по этому примеру
+                neuron.Train(km, miles);
+                // выводим информацию о каждой 100000 операции
+                if (i % 100000 == 0)
+                {
+                    Console.WriteLine($"Итерация {i} \t Ошибка \t {neuron.LastError}");
+
+                }
+                // сравниваем ошибку со сглаживанием  - больши или меньше сглаживания
+            } while (neuron.LastError > neuron.Smoothing || neuron.LastError < -neuron.Smoothing);
+            Console.WriteLine("Обучение завершено");
+
+
+
+            Console.WriteLine($"{neuron.ProcessingInputData(100)}  миль в {100} километрах");
+
+            Console.WriteLine($"{neuron.ProcessingInputData(540)}  миль в {540} километрах");
+
+            // сколько километров в 10 милях
+
+            Console.WriteLine($"{neuron.RestoreInputData(10)}  км в {10} милях");
+
         }
     }
 }
