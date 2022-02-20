dc30bc48c388a89918074a0b87f7152579e9d2a8
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-20 22:20:51 +0300

    var result

--- LINQCODEBLOG/Program.cs
+++ LINQCODEBLOG/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQCODEBLOG
 {
@@ -6,7 +8,23 @@ namespace LINQCODEBLOG
     {
         static void Main(string[] args)
         {
-            
+            // данные из бд, коллекции, наборы данных
+            List<int> collection = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                collection.Add(i); 
+            }
+
+            var result = from item in collection
+                         where item > 5
+                         select item;
+
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
