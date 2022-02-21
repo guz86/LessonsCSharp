14b3bbbcab012ab2340b2771fe5a74ae28a129d3
Pavel Gordienko <guzmo6@rambler.ru>
2022-02-21 21:53:09 +0300

    List<Product> products

--- LINQCODEBLOG/Program.cs
+++ LINQCODEBLOG/Program.cs
@@ -27,6 +27,7 @@ namespace LINQCODEBLOG
             }
 
             // с помощью методов расширения . со стрелочками
+
             //=> лямбда выражение, - такой что ... условие
             var result2 = collection.Where(item => item > 6);
 
@@ -34,6 +35,72 @@ namespace LINQCODEBLOG
             {
                 Console.WriteLine(item);
             }
+
+
+
+            // классы
+
+            //List<Product> products = new List<Product>();
+            Random rand = new Random();
+            List<Product> products = new List<Product>();
+            for (int i = 1; i <= 10; i++)
+            {
+                
+                Product product = new Product()
+                {
+                    Name = "product:" + i,
+                    Energy = rand.Next(10, 1000) 
+                };
+                products.Add(product);
+            }
+
+            foreach (var product in products)
+            {
+                //Console.WriteLine(product.Name+"\t"+product.Energy);
+                Console.WriteLine(product);
+            }
+
+            var productEnergyMore500 = from product in products
+                                       where product.Energy > 500
+                                       orderby product.Energy
+                                       select product;
+
+            Console.WriteLine("\nProduct more then 500 Energy(orderby)\n");
+            foreach (var product in productEnergyMore500)
+            {
+                Console.WriteLine(product);
+            }
+            // с помощью методов расширения
+
+            var productEnergyMore500800Method = products.Where(product => product.Energy > 500
+            && product.Energy < 800);
+            Console.WriteLine("\nProduct more then 500 Energy and less than 800\n");
+            foreach (var product in productEnergyMore500800Method)
+            {
+                Console.WriteLine(product);
+            }
+
+            Console.WriteLine();
+
+            // преобразование одного типа в другой
+            // из списка продуктов, список Energy
+            var selectProducts = from product in products
+                                 select product.Energy;
+            Console.WriteLine(products);
+            Console.WriteLine(selectProducts);
+
+            var selectProductsMethod = products.Select(product => product.Energy);
+
+            foreach (var item in selectProducts)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in selectProductsMethod)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
         }
     }
 }
