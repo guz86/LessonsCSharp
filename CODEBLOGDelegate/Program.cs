c1de22a5268ece4b6c7e24c13a3d9ab070075ce0
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-03 19:38:16 +0300

    myDelegate

--- CODEBLOGDelegate/Program.cs
+++ CODEBLOGDelegate/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace CODEBLOGDelegate
+namespace DelegateCODEBLOG
 {
+    // Указатель на метод. Сигнатура делегата опеределяем методы с какой сигнатурой в нем будут использаться
+    //
+    public delegate void MyDelegate();
+    // не принимает аргументов, не возвращает значений
     class Program
     {
         static void Main(string[] args)
         {
-            
+            MyDelegate myDelegate = Action1;
+            myDelegate();
+
+        }
+
+        public static void Action1()
+        {
+            Console.WriteLine("Action1");
         }
     }
 }
