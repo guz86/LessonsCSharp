08f1152caa372cbafc89cfd04f51d7ef8a725949
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-03 21:49:46 +0300

    myDelegate2

--- CODEBLOGDelegate/Program.cs
+++ CODEBLOGDelegate/Program.cs
@@ -11,13 +11,34 @@ namespace DelegateCODEBLOG
         static void Main(string[] args)
         {
             MyDelegate myDelegate = Action1;
+            // второй метод в переменную делегата
+            myDelegate += Action2;
             myDelegate();
 
+            Console.WriteLine();
+            // создать делегат и добавить в него метод
+            MyDelegate myDelegate2 = new MyDelegate(Action2);
+            myDelegate2.Invoke();
+            Console.WriteLine();
+
+
+            // в один делегат можно добавить один и тот же метод несколько раз
+            myDelegate2 += Action2;
+            myDelegate2 += Action2;
+            // удалить
+
+            myDelegate2 -= Action2;
+
+            myDelegate2.Invoke();
         }
 
         public static void Action1()
         {
             Console.WriteLine("Action1");
+        }
+        public static void Action2()
+        {
+            Console.WriteLine("Action2");
         }
     }
 }
