d0c42982037cc206ec6484337b2d079f1e903fd1
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-07 02:11:39 +0300

    noParamters

---
+++ DelegatesCodaza/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DelegatesCodaza
+{
+    class Program
+    {
+        public delegate void MyDelegate();
+
+        static void Main(string[] args)
+        {
+            // MyDelegate myDelegate = new(ShowMessage);
+            MyDelegate myDelegate = ShowMessage;
+            myDelegate();
+
+        }
+        
+        private static void ShowMessage() {
+            Console.WriteLine("Message");
+        }
+    }
+}
