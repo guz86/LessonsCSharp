92baf2f32a603e1af2a4626c2383cfdf3f0d9358
Pavel Gordienko <guzmo6@rambler.ru>
2022-03-07 03:01:25 +0300

    StockExchangeMonitor

--- DelegatesExempleCodaza/Program.cs
+++ DelegatesExempleCodaza/Program.cs
@@ -10,8 +10,21 @@ namespace DelegatesExempleCodaza
             // получив цену библиотека StockExchangeMonitor будет сообщать ее всем желающим
             // наше приложение тоже будет получать цену и будет выводить ее на консоль в реальном времени
 
+            //создаем монитор
+            StockExchangeMonitor stockExchangeMonitor = new StockExchangeMonitor();
+
+            // регистрируем метод
+            stockExchangeMonitor.PriceChangeHandler = ShowPrice;
+            // запускаем
+            stockExchangeMonitor.Start();
 
 
+        }
+
+        // метод который будет выводить текущую цену на консоль
+        public static void ShowPrice(int price)
+        {
+            Console.WriteLine(price);
         }
     }
 }
--- DelegatesExempleCodaza/StockExchangeMonitor.cs
+++ DelegatesExempleCodaza/StockExchangeMonitor.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DelegatesExempleCodaza
 {
-    class StockExchangeMonitor
+    public class StockExchangeMonitor
     {
+        public delegate void PriceChange(int price);
+        //price текущая цена на акцию
 
+        // добавим свойство через которое можно будет зарегистрировать ссылку на метод
+        // через этот код внешний код сможет добавять методы для оповещения о новой цене
+        public PriceChange PriceChangeHandler { get; set; }
+        // метод чтобы запустить наш монитор
+        public void Start() {
+            // опрос биржи
+            while (true)
+            {
+                // получаем текущую цену
+                int bankOfAmericaPrice = (new Random()).Next(100);
+                // оповестим всех желающих о новой цене
+                PriceChangeHandler(bankOfAmericaPrice);
+
+                Thread.Sleep(1500);
+            }
+        }
     }
 }
